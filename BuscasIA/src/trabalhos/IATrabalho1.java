3462fdd8a884156f1742683fd3366a1eaa44b7ff
heloisakopsch <manitur.heloisa@gmail.com>
2015-03-15 15:03:58 -0300

    commit da extensão da classe Estado

--- BuscasIA/src/trabalhos/IATrabalho1.java
+++ BuscasIA/src/trabalhos/IATrabalho1.java
@@ -1,10 +1,32 @@
 package trabalhos;
 
-public class IATrabalho1 {
+import java.util.List;
+
+import busca.Estado;
+
+public class IATrabalho1 implements Estado {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+	}
+
+	@Override
+	public int custo() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean ehMeta() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public List<Estado> sucessores() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
